fcef8f8fe087345c7312bb345b2db3e2bb41d06d
tmychow <tmychow@gmail.com>
2025-08-18 17:56:57 -0700

    Improved plot

--- plot_correctness.py
+++ plot_correctness.py
@@ -4,12 +4,23 @@ import matplotlib.pyplot as plt
 train = pd.read_csv('run_26_train_correctness.csv')
 val = pd.read_csv('run_26_val_correctness.csv')
 
+plt.figure()
 plt.plot(train['Step'], train['train/correctness'], label='Train')
-plt.plot(val['Step'], val['val/correctness'], label='Val')
 plt.xlabel('Step')
 plt.ylabel('Correctness')
 plt.legend()
 plt.tight_layout()
-plt.savefig('correctness.png')
+plt.savefig('train_correctness.png')
+
+plt.figure()
+plt.plot(val['Step'], val['val/correctness'], label='Validation')
+plt.axhline(y=0.8, color='red', linestyle=':', linewidth=1.5, label='GPT 4.1')
+plt.xlabel('Step')
+plt.ylabel('Correctness')
+plt.legend()
+plt.tight_layout()
+plt.savefig('val_correctness.png')
+
+plt.close('all')
 
 
